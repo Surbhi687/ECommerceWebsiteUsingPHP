8d3009724e53a054dbcffa953ceb28cdb75d87ab
Surbhi687 <surbhidkumar@gmail.com>
2025-11-16 15:33:06 +0530

    Initial commit

--- db.php
+++ db.php
@@ -1,17 +1,12 @@
 <?php
-
-$servername = "localhost";
+$servername = "localhost:3307"; 
 $username = "root";
 $password = "";
 $db = "onlineshop";
 
-// Create connection
-$con = mysqli_connect($servername, $username, $password,$db);
+$con = mysqli_connect($servername, $username, $password, $db);
 
-// Check connection
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-
 ?>
